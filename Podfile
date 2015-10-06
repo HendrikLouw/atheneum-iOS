c65474e2377f7c9891bfaabd7e6e05d10e9e040c
Hendrik Louw <hlouw@thoughtworks.com>
2015-10-06 13:17:47 +0200

    Initial commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'atheneum' do
+
+end
+
+target 'atheneumTests' do
+
+end
+
+target 'atheneumUITests' do
+
+end
+
---
+++ atheneum/BookshelfController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  atheneum
+//
+//  Created by Hendrik Louw on 2015/10/06.
+//  Copyright © 2015 Thoughtworks. All rights reserved.
+//
+
+import UIKit
+
+class BookshelfController: UIViewController {
+    
+    @IBOutlet weak var prototypeImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
